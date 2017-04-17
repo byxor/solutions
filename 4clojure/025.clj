a177368899b8f81193226df8e310f50696c48134
Brandon Ibbotson <brandnewbyxor@gmail.com>
2017-04-17 21:03:23 +0100

    Update 025.clj

--- 4clojure/025.clj
+++ 4clojure/025.clj
@@ -1,6 +1,1 @@
-(fn get_odds [collection]
-  (if (= collection [])
-    (list)
-    (if (= (mod (first collection) 2) 1)
-       (conj (get_odds (rest collection)) (first collection))
-       (get_odds (rest collection)))))
+(fn [coll] (filter odd? coll))
