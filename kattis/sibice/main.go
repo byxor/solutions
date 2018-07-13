6f2b31fff0c7b237cdda60f785861c263e10493a
byxor <brandnewbyxor@gmail.com>
2018-07-13 23:26:44 +0100

    Optimize

--- kattis/sibice/main.go
+++ kattis/sibice/main.go
@@ -7,17 +7,24 @@ import (
 	"strconv"
 )
 
-func main() {
-	var matches int
-	var width, height float64
-	fmt.Scanf("%d %f %f\n", &matches, &width, &height)
-	boxLength := width*width + height*height
+var numMatches int
+var width, height float64
 
-	scanner := bufio.NewScanner(os.Stdin)
-	for i := 0; i < matches; i++ {
+var boxLength float64
+var matchLength float64
+
+var scanner *bufio.Scanner
+var line string
+
+func main() {
+	fmt.Scanf("%d %f %f\n", &numMatches, &width, &height)
+	boxLength = width*width + height*height
+	scanner = bufio.NewScanner(os.Stdin)
+
+	for i := 0; i < numMatches; i++ {
 		scanner.Scan()
-		line := scanner.Text()
-		matchLength, _ := strconv.Atoi(line)
+		line = scanner.Text()
+		matchLength, _ = strconv.ParseFloat(line, 10)
 		if float64(matchLength*matchLength) <= boxLength {
 			fmt.Println("DA")
 		} else {
