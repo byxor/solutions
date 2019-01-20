c00826ec0e1b3aab7cde1570bcc7aca636060859
byxor <brandnewbyxor@gmail.com>
2019-01-20 22:51:52 +0000

    Golf

--- kattis/hissingmicrophone/hissingmicrophone.c
+++ kattis/hissingmicrophone/hissingmicrophone.c
@@ -1,8 +1,1 @@
-char*M="no hiss",m[31],i,s=0;
-main(){
- gets(m);
- for(;m[i]&&s<2;){
-  s=m[i++]^'s'?0:s+1;
- }
- puts(M+(s>1)*3);
-}
+char*M="no hiss",m[31],i,s=0;main(){gets(m);for(;s<2&&m[i];)s=m[i++]^'s'?0:s+1;puts(M+(s>1)*3);}
